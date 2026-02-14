6a34d82ad9209191f5f85fd43b0a85126e9e91d2
balriobdb <mauriciofreitasb3@gmail.com>
2026-02-13 21:47:44 -0300

    Remoção de comentários

--- objetos/ProdutoController.php
+++ objetos/ProdutoController.php
@@ -8,15 +8,13 @@ Class ProdutoController {
     private $produto;
 
     public function __construct(){
-        // Cria a conexão com o banco de dados
+
         $banco = new Database();
         $this->bd = $banco->conectar();
 
-        // Instancia o objeto Produto passando a conexão
         $this->produto = new Produto($this->bd);
     }
 
-    // Método principal que retorna todos os produtos
     public function index(){
         return $this->produto->LerTodos();
     }
